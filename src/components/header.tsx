2c485bd9f902ca898e4df647cb4f11584e09358e
Zeno Leonardi <zenoelioleone@gmail.com>
2025-01-26 23:59:07 +0100

    Better nav for moblie

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,9 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/app/assets/logo.png";
@@ -26,14 +32,16 @@ export const routes = [
 export function Header() {
   return (
     <header className="flex px-4 w-full mb-12 top-0 sticky bg-background z-10">
-      <nav className="pt-2 pb-2 font-medium flex space-x-2">
-          <Image
-            src={logo}
-            alt={"Logo"}
-            width={64}
-            className="brightness-0 invert"
-          ></Image>
-        <div className="space-x-2 pt-4">
+      <nav className="pt-2 pb-2 font-medium flex space-x-2 w-full justify-between">
+        <Image
+          src={logo}
+          alt={"Logo"}
+          width={64}
+          height={64}
+          className="brightness-0 invert"
+        ></Image>
+
+        <div className="space-x-2 pt-4 hidden sm:flex">
           {routes.map((route) => {
             return (
               <Button variant="headerLink" size="lg" key={route.name}>
@@ -41,6 +49,24 @@ export function Header() {
               </Button>
             );
           })}
+        </div>
+        <div className="flex sm:w-0 sm:hidden pt-4 pr-10">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant={"ghost"}>
+                <span className="text-4xl">&#x2261;</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40">
+              {routes.map((route) => {
+                return (
+                  <Button variant="headerLink" size="lg" key={route.name}>
+                    <Link href={route.href}>{route.name}</Link>
+                  </Button>
+                );
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </nav>
     </header>
