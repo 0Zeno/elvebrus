9928b99a47fe7985e9637f6a45d651bec6d80e45
Zeno Leonardi <zenoelioleone@gmail.com>
2025-01-28 14:27:06 +0100

    Fix dates

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -6,7 +6,7 @@ export function HeroSection() {
           Stanghelle
         </h2>
         <h2 className="text-2xl md:text-4xl md:drop-shadow-[3px_3px_0px_rgba(0,0,0,1)] drop-shadow-[2px_2px_0px_rgba(0,0,0,1)]">
-          19.-20. juni
+          18.-19. juni
         </h2>
       </div>
       <h1 className="text-6xl text-center md:text-9xl pb-4 pt-10 md:drop-shadow-[5px_5px_0px_rgba(0,0,0,1)] drop-shadow-[3px_3px_0px_rgba(0,0,0,1)]">
