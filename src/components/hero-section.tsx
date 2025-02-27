2b1fd4c0a987392f62c9cded1c7dd5024f841f07
Zeno Leonardi <zenoelioleone@gmail.com>
2025-02-27 23:13:20 +0100

    Improve hero

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,7 +1,7 @@
 export function HeroSection() {
   return (
-    <div className="pt-8 md:pt-24">
-      <div className="flex justify-between">
+    <div className="pt-8 md:pt-12">
+      <div className="flex justify-between md:px-4">
         <h2 className="text-lg md:text-4xl md:drop-shadow-[3px_3px_0px_rgba(0,0,0,1)] drop-shadow-[2px_2px_0px_rgba(0,0,0,1)]">
           Stanghelle
         </h2>
@@ -9,7 +9,7 @@ export function HeroSection() {
           18.-19. juni
         </h2>
       </div>
-      <h1 className="text-8xl text-center md:text-[248px] pb-4 sm:pt-4 md:drop-shadow-[5px_5px_0px_rgba(0,0,0,1)] drop-shadow-[3px_3px_0px_rgba(0,0,0,1)]">
+      <h1 className="text-8xl font-bold text-center md:text-[268px] pb-4 sm:pt-4 md:drop-shadow-[5px_5px_0px_rgba(0,0,0,1)] drop-shadow-[3px_3px_0px_rgba(0,0,0,1)]">
         ELVEBRUS
       </h1>
     </div>
