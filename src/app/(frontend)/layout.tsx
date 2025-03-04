cd8fb089062745b2cdbe600d067b13dc851608b8
Zeno Leonardi <zenoelioleone@gmail.com>
2025-03-04 12:38:14 +0100

    SEO improvements

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,6 +3,15 @@ import "../globals.css";
 import { Footer } from "@/components/footer";
 import { SanityLive } from "@/sanity/lib/live";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Elvebrusfestivalen",
+  description: "Musikkfestival i Agnavika på Stanghelle 18.-19. Juli 2025.",
+  keywords:
+    "Elvebrusfestivalen, musikkfestival, festival Norge, konserter, live musikk, festival 2025, Stanghelle, Agnavika, Elvebrus, elvebrus",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
